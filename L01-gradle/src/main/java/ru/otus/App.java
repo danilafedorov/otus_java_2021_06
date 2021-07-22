18255db20bc4d4f5d30d8f3ea636709149ab624a
Danila Fedorov <dyu.fedorov@cdek.ru>
2021-07-22 21:34:55 +0700

    add gradle project

---
+++ L01-gradle/src/main/java/ru/otus/App.java
@@ -0,0 +1,14 @@
+package ru.otus;
+
+import com.google.common.collect.Lists;
+
+import java.util.Collection;
+
+public class App {
+
+    public static void main(String[] args) {
+        Collection<String> testCollection = Lists.newArrayList("Test", "Test2", "Test3");
+        System.out.println("Hello world! " + testCollection);
+    }
+
+}
