107547116a857e41463c45fdcf79c672f4df1654
fedorov.d <fedorov@improveitgroup.com>
2021-07-29 17:51:46 +0700

    remove examples

--- hw01-gradle/src/main/java/ru/otus/HelloOtus.java
+++ hw01-gradle/src/main/java/ru/otus/HelloOtus.java
@@ -4,7 +4,7 @@ import com.google.common.collect.Lists;
 
 import java.util.Collection;
 
-public class App {
+public class HelloOtus {
 
     public static void main(String[] args) {
         Collection<String> testCollection = Lists.newArrayList("Test", "Test2", "Test3");
